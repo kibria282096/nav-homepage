d773f95a49ec539370e4aa1405feb4aa586ed938
kibria282096 <jucse282096@gmail.com>
2024-01-14 22:39:42 +0600

    first commit

---
+++ src/components/Carousel/MyCarosel.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+
+import metro1 from "./images/metro1.jpg";
+import metro2 from "./images/metro2.jpg";
+import metro3 from "./images/metro3.jpg";
+
+function MyCarosel() {
+  return (
+    <div>
+      <Carousel>
+        <Carousel.Item>
+          <img className="d-block w-100" src={metro1} alt="First slide" />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={metro2} alt="Second slide" />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={metro3} alt="Third slide" />
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+}
+
+export default MyCarosel;
